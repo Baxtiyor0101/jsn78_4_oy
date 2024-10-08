5732e662ff39aa821893c88dd9831d78fa3c696b
Baxtiyor0101 <oqonboyevb@gmail.com>
2024-10-08 15:53:51 +0500

    datatypes operators

---
+++ lesson2/app.js
@@ -0,0 +1,171 @@
+// alert("hello n78 are you happy starting js");
+// console.log("helloo js application");
+// confirm("are yuo single?");
+// prompt("what is your name?");
+
+// bitta qator uchun
+
+/* kop qatorlarni comment 
+kjcsdkjc 
+sdcnkjdsc
+sdcnjsdcs
+dcsdncsd
+csdcs
+d
+*/
+
+/////////////////////////  Ozgaruvchilar nomlash
+
+// let var const
+// let num2 = 23;
+// let nums =89;
+// num = 22;
+// console.log(num);
+// {
+//   var nums = 44;
+// }
+// console.log(nums);
+
+// .//////////////////////////// data types
+
+// primative
+// Number 1,2,3,-23,2.2,-222.2
+// String " dvdfv😂"
+// Boolean true false
+// undefined
+// null
+// Symbol
+// BigInt
+// Up to 9007199254740991 +(2(53)-1)
+
+// and
+
+// Down to -9007199254740991 -(2(53)-1).
+
+// nonprimitive(referance)
+// Object
+// Array
+
+// let person = {
+//   name: "John",
+//   age: 30,
+//   isMarried: true,
+//   address: {
+//     street: "123 Main St",
+//     city: "New York",
+//     zip: {
+//       code: "10001",
+//       state: "NY",
+//     },
+//   },
+// };
+
+// let fuits = [
+//   "apple",
+//   "orange",
+//   "kiwi",
+//   23,
+//   true,
+//   {
+//     name: "John",
+//     age: 30,
+//     isMarried: true,
+//     address: {
+//       street: "123 Main St",
+//       city: "New York",
+//       zip: {
+//         code: "10001",
+//         state: "NY",
+//       },
+//     },
+//   },
+// ];
+
+// console.log(fuits);
+
+// //////////////////////////
+
+// console.log(names);
+// console.log(isAccepted);
+
+// let f_name = prompt("What is your full name");
+// let age = prompt("What is your age");
+// let isMarried = confirm("What is your marrige status");
+// let city = prompt("What is your city");
+// let region = prompt("What is your region");
+// let phone_number = prompt("What is your phone number");
+
+// let user = {
+//   name: f_name,
+//   age: age,
+//   isMarried: isMarried,
+//   address: {
+//     city: city,
+//     region: region,
+//   },
+//   phoneNumber: phone_number,
+// };
+
+// console.log(user);
+
+// let names = prompt("what is your full name");
+// let isAccepted = confirm("Are you sure you want to accept");
+// student
+
+// name
+// age
+// grade
+// subjects
+// grades
+// averageGrade
+// highestGrade
+// lowestGrade
+// address
+// phone number
+// email
+// courses
+// city
+// isMarried
+// hobbies
+// familyStatus
+// familyMembers (object)
+// siblings
+// StudentOf
+
+// let student = {
+//     name: names,
+//     isAccepted: isAccepted,
+//     age: Number(prompt("What is your age")),
+//     grade: prompt("What is your grade"),
+//     subjects: [prompt("Enter your first subject"), prompt("Enter your second subject")],
+//     grades: [],
+//     averageGrade: 0,
+//     highestGrade: 0,
+//     lowestGrade: 100,
+//     address: {
+//         city: prompt("What is your city"),
+//         region: prompt("What is your region"),
+//     },
+//     phoneNumber: prompt("What is your phone number"),
+//     email: prompt("What is your email"),
+//     courses: [],
+//     city: prompt("What is your city"),
+//     isMarried: confirm("Are you married"),
+//     hobbies: [prompt("Enter your first hobby"), prompt("Enter your second hobby")],
+//     familyStatus: prompt("What is your family status"),
+//     familyMembers: {
+//         mother: {
+//             name:
+// }
+
+// ////////////////////////////math operators
+// - + * /
+
+let a = prompt("Enter your first side");
+let b = prompt("Enter your second side");
+
+// let p = 2 * (+a + +b); // "5"+"6"="56"
+// console.log(p);
+
+let s = a * b;
+console.log(s);
---
+++ lesson2/homework.js
@@ -0,0 +1,13 @@
+// Simple Calculator
+// a,b,operator
+// console.log(a operator b);
+// console.log(5 + 6);
+
+// Personal Information Form // kamida 10ta bolishi kk
+
+// Shopping Total Calculator kamida 5ta product (olma behi karoshka...)
+
+// Avarage Grade Calculator kamida 5ta fan(math,phisics,english ...)
+
+// Temperature Converter
+// (32 °F − 32) × 5/9 = 0 °C
