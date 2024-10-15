1287dfc28db68a3da7412eab32a6e3278138d330
Baxtiyor0101 <oqonboyevb@gmail.com>
2024-10-15 12:44:36 +0500

    operator

--- lesson2/homework.js
+++ lesson2/homework.js
@@ -2,9 +2,7 @@
 // a,b,operator
 // console.log(a operator b);
 // console.log(5 + 6);
-
 // Personal Information Form // kamida 10ta bolishi kk
-
 // Shopping Total Calculator kamida 5ta product (olma behi karoshka...)
 
 // Avarage Grade Calculator kamida 5ta fan(math,phisics,english ...)
---
+++ lesson3/app.js
@@ -0,0 +1,195 @@
+// Type conversion
+// string, boolean => number
+let res = "4" / 2;
+// console.log(typeof ("a"/2));
+// NaN => Not a Number
+// let a;
+// console.log(a);
+// console.log(Number(a));
+// console.log(Number("ss"));
+// console.log(Number(true));
+// console.log(Number(false));
+// console.log(Number(null));
+// console.log(Number(undefined));
+// console.log(Number({}));
+// console.log(Number([]));
+// let test =  Symbol("")
+// console.log(+true);
+
+// let names = "bobur"
+// let surname="qodirov"
+// console.log(2+names);
+
+// Number() constructor function
+// let a = "-12.56";
+// console.log(parseInt(a));
+// console.log(parseFloat(a));
+
+// console.log(+a);
+// console.log(-a);
+
+// String() constructor function
+// number, boolean ...=> string
+// let a = [2];
+// console.log(String(a));
+
+// console.log(String(a));
+// console.log(a + "");
+
+// Number => String
+// console.log(a.toString());
+// console.log(a.toFixed(5));
+
+//
+
+// Operators
+// Arithmetic
+// +, -, *, /, %, ++, --, **, // => comment
+// let num1 = 23;
+// // let num2 = 3;
+// num1++;
+// num1++;
+// console.log(num1--);
+// console.log(num1);
+
+// let num1 = 3;
+// console.log(num1 ** 4);
+// console.log(2);
+
+// Comparison (Taqqoslash)
+// >, <, <=, >=, ==, ===, !=, !==
+// console.log(1 > 1);
+// console.log(1 < 1);
+// console.log(1 >= 1);
+// console.log(1 == 1); // equality
+// console.log(1 === 1); //strict equality
+// console.log(1 != 31); //not equal
+// console.log(1 !== 1); //not strict equal
+// console.log("2" === "22");
+
+// Logical Operators
+// and => &&, or ||, not !
+// && => birinchi falsy value qidiradi topolmasa oxirgi truthy valueni oladi
+// console.log(a==3  && b==4);
+// let a = 3;
+// let b;
+// console.log(a == 3 && b == 4);
+// || => birinchi truthy value qidiradi topolmasa oxirgi falsy valueni oladi
+// console.log(0 || "" || null);
+
+// console.log(!"d");
+
+// console.log(Boolean(true));
+// console.log(Boolean(false));
+// console.log(Boolean(0));
+// console.log(Boolean(1));
+// console.log(Boolean(""));
+// console.log(Boolean(" "));
+// console.log(Boolean("a"));
+// console.log(Boolean(null));
+// console.log(Boolean(undefined));
+// console.log(Boolean({}));
+// console.log(Boolean([]));
+// console.log(Boolean(Symbol("")));
+// console.log(Boolean(0n));
+// console.log(Boolean(-3));
+// console.log(Boolean(NaN));
+
+// Falsy values
+// 0
+// false
+// null
+// undefined
+// ""
+// 0n
+// -0
+// NaN
+//
+
+// Truthy values
+//  qolgan hamma malumotlar trithy
+
+// not
+// console.log(!true)
+// console.log(!false)
+
+// or
+// console.log(true || true)
+// console.log(true || false);
+// console.log(false || true);
+// console.log(false || false);
+
+// and
+// console.log(true && true);
+// console.log(true && false);
+// console.log(false && true);
+// console.log(false && false);
+
+// to boolean
+// Boolean() Constructor Function
+// Falsy => 0, -0, NaN, '', "", null, undefined, false
+// Truthy => others, " ", 'a'
+// let a = -Infinity;
+
+// console.log(Boolean(a));
+// console.log(!!a);
+
+// console.log("A" > "a");
+
+// let a = 65;
+
+// console.log(a.toString(8));
+let r;
+console.log((2 && 4 && false && 4) || (true && {}));
+console.log(8 && r && 6 && 45 && 0 && [] && 43);
+
+// Object creation
+// let person = { name: "Eshmat", age: 32 };
+// let key = prompt("Nimani o'qimiz? ")
+
+// read
+// dot notation
+// console.log(person.age);
+// bracket notation
+// console.log(person[key]);
+
+// add
+// console.log(person);
+// person.email = "eshmat@gmail.com";
+// person["phone"] = "+998901234567";
+// console.log(person);
+
+// update
+// console.log(person);
+// person.name = "Toshmat";
+// person["age"] = 18;
+// console.log(person);
+
+// delete
+// console.log(person);
+// delete person.age;
+// delete person["age"];
+// console.log(person);
+
+// creation
+// let a = ["apple", "banana"];
+
+// read
+// console.log(a[0]);
+// console.log(a[1]);
+
+// add
+// console.log(a);
+// a[2] = "Cherry";
+// console.log(a);
+
+// update
+// console.log(a);
+// a[1] = "Cherry";
+// console.log(a);
+
+// delete
+// console.log(a);
+// delete a[1];
+// console.log(a);
+// console.log(a[1]);
